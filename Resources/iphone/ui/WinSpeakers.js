668a69ef1aa8b003cb7c4c74eda059cda647b8ff
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-17 00:21:56 -0300

    Create Speakers Window to iOS and Android (just initial template)

---
+++ Resources/iphone/ui/WinSpeakers.js
@@ -0,0 +1,31 @@
+(function() {
+  var Window;
+
+  Window = function() {
+    var Model, model, rows, self, speaker, speakers, tableView, ui;
+    Model = require("/lib/Model");
+    ui = require("/ui/components");
+    model = new Model();
+    rows = [];
+    self = new ui.createWindow({
+      title: L("speakers")
+    });
+    tableView = new ui.createTableView();
+    self.add(tableView);
+    speakers = model.getSpeakers();
+    rows = (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = speakers.length; _i < _len; _i++) {
+        speaker = speakers[_i];
+        _results.push(ui.createTalkerRow(speaker));
+      }
+      return _results;
+    })();
+    tableView.setData(rows);
+    return self;
+  };
+
+  module.exports = Window;
+
+}).call(this);
