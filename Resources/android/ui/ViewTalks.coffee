ebe4d0a7b9f5cd502f3ad28c32a1d52e0558a3bd
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-18 13:32:46 -0300

    Clean code

--- Resources/android/ui/ViewTalks.coffee
+++ Resources/android/ui/ViewTalks.coffee
@@ -1,20 +1,8 @@
 View = ->
 
-	# Requirements
-	Model = require "/lib/Model"
-	ui = require "/ui/components"
-
-	# Instance Model object
-	model = new Model()
-	rows = []
-
 	# Create the View
-	self = new ui.createView
+	self = Ti.UI.createView
 		backgroundColor: "#0000FF"
-
-	# Create TableView
-	tableView = new ui.createTableView()
-	self.add tableView
 
 	self
 
--- Resources/android/ui/ViewTalks.js
+++ Resources/android/ui/ViewTalks.js
@@ -2,16 +2,10 @@
   var View;
 
   View = function() {
-    var Model, model, rows, self, tableView, ui;
-    Model = require("/lib/Model");
-    ui = require("/ui/components");
-    model = new Model();
-    rows = [];
-    self = new ui.createView({
+    var self;
+    self = Ti.UI.createView({
       backgroundColor: "#0000FF"
     });
-    tableView = new ui.createTableView();
-    self.add(tableView);
     return self;
   };
 
