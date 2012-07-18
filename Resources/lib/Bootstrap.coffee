a420731297fb473ce782c31f11242922a70513fb
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-18 13:29:22 -0300

    Create a Bootstrap file to get JSON files informations and put it in Properties

---
+++ Resources/lib/Bootstrap.coffee
@@ -0,0 +1,28 @@
+exports.setInitialConfigutarion = ->
+
+	extension = "json"
+	files = [
+		{ type: "Object", name: "Config" }
+		{ type: "Array", name: "Speakers" }
+		{ type: "Array", name: "Talks"}
+	]
+
+	for file in files
+		do (file) ->
+
+			# Get physical file
+			property_name = file.name.toLowerCase()
+			file_name = "#{file.name}.#{extension}"
+			physical_file = Ti.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, file_name);
+
+			# If file exists
+			if (physical_file.exists())
+				
+				# Get file content and JSON parse it
+				file_content = JSON.parse physical_file.read().text
+
+				# If type is Object, use setObject
+				if file.type == "Object"
+					Ti.App.Properties.setObject(property_name, file_content)
+				else if file.type == "Array"
+					Ti.App.Properties.setList(property_name, file_content)
---
+++ Resources/lib/Bootstrap.js
@@ -0,0 +1,39 @@
+(function() {
+
+  exports.setInitialConfigutarion = function() {
+    var extension, file, files, _i, _len, _results;
+    extension = "json";
+    files = [
+      {
+        type: "Object",
+        name: "Config"
+      }, {
+        type: "Array",
+        name: "Speakers"
+      }, {
+        type: "Array",
+        name: "Talks"
+      }
+    ];
+    _results = [];
+    for (_i = 0, _len = files.length; _i < _len; _i++) {
+      file = files[_i];
+      _results.push((function(file) {
+        var file_content, file_name, physical_file, property_name;
+        property_name = file.name.toLowerCase();
+        file_name = "" + file.name + "." + extension;
+        physical_file = Ti.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, file_name);
+        if (physical_file.exists()) {
+          file_content = JSON.parse(physical_file.read().text);
+          if (file.type === "Object") {
+            return Ti.App.Properties.setObject(property_name, file_content);
+          } else if (file.type === "Array") {
+            return Ti.App.Properties.setList(property_name, file_content);
+          }
+        }
+      })(file));
+    }
+    return _results;
+  };
+
+}).call(this);
