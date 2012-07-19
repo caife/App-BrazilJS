ce83eb59ac85cbb4dcc1cdc5678f49d70732f5e7
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-19 17:26:29 -0300

    Get talks, contruct rows and show it in TableView

--- Resources/android/ui/ViewTalks.coffee
+++ Resources/android/ui/ViewTalks.coffee
@@ -1,8 +1,27 @@
 View = ->
 
+	# Requirements
+	Model = require "/lib/Model"
+	ui = require "/ui/components"
+
+	# Instance Model object
+	model = new Model()
+	rows = []
+
 	# Create the View
-	self = Ti.UI.createView
-		backgroundColor: "#0000FF"
+	self = ui.createView
+		backgroundColor: "#FFFFFF"
+
+	# Get data and set to rows Array
+	talks = model.getTalks()
+	rows = (ui.createTalkRow talk for talk in talks)
+
+	# Create TableView
+	tableView = new ui.createTableView
+		separatorColor: "#BEBEBE"
+		data: rows
+
+	self.add tableView
 
 	self
 
--- Resources/android/ui/ViewTalks.js
+++ Resources/android/ui/ViewTalks.js
@@ -2,10 +2,29 @@
   var View;
 
   View = function() {
-    var self;
-    self = Ti.UI.createView({
-      backgroundColor: "#0000FF"
+    var Model, model, rows, self, tableView, talk, talks, ui;
+    Model = require("/lib/Model");
+    ui = require("/ui/components");
+    model = new Model();
+    rows = [];
+    self = ui.createView({
+      backgroundColor: "#FFFFFF"
     });
+    talks = model.getTalks();
+    rows = (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = talks.length; _i < _len; _i++) {
+        talk = talks[_i];
+        _results.push(ui.createTalkRow(talk));
+      }
+      return _results;
+    })();
+    tableView = new ui.createTableView({
+      separatorColor: "#BEBEBE",
+      data: rows
+    });
+    self.add(tableView);
     return self;
   };
 
