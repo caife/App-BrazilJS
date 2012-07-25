856db068eddb112125629c4c1d9f6f44c9976336
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-25 20:41:39 -0300

    Support different bundles

--- run.sh
+++ run.sh
@@ -1,3 +1,54 @@
+#!/bin/sh
+
+# Separate parameters (thanks @felipek)
+for param in $@; do eval "${param%%=*}=${param##*=}"; done
+
+# If have a specific build
+if [ ! -z "${bundle}" ]; then
+	if [ -d ./Bundles/"${bundle}"/ ]; then
+
+		# Clean all build folder
+		rm -Rf ./build/
+
+		# Clean images
+		rm -Rf ./Resources/images/*
+
+		printf "Build folder deleted.\n"
+
+		# Move all icons and splashs to root directory
+		# iOS
+		cp ./Bundles/"${bundle}"/app/iphone/appicon.png ./Resources/iphone/
+		cp ./Bundles/"${bundle}"/app/iphone/appicon@2x.png ./Resources/iphone/
+		cp ./Bundles/"${bundle}"/app/iphone/Default.png ./Resources/iphone/
+		cp ./Bundles/"${bundle}"/app/iphone/Default@2x.png ./Resources/iphone/
+		printf "iOS resources copied.\n"
+
+		# Android
+		cp ./Bundles/"${bundle}"/app/android/appicon.png ./Resources/android/
+		cp ./Bundles/"${bundle}"/app/android/default.png ./Resources/android/
+		printf "Android resources copied.\n"
+
+		# Move Bundle file to root directory
+		cp ./Bundles/"${bundle}"/app/tiapp.xml ./
+		printf "Bundle (${bundle}) resources copied.\n"
+
+		# Copy Images folder
+		cp -r ./Bundles/"${bundle}"/images/* ./Resources/images/
+		printf "Images copied.\n"
+
+		# Copy Data folder
+		cp -r ./Bundles/"${bundle}"/data/* ./Resources/data/
+		printf "Data copied.\n"
+
+	else
+		printf "The bundle ("${bundle}") not exists.\n"
+		exit 1
+	fi
+else
+	printf "No one file was copied.\n"
+fi
+
+# Compile CoffeeScript
 # Default
 coffee -c -o ./Resources/lib/ ./Resources/lib/*.coffee
 coffee -c -o ./Resources/ui/ ./Resources/ui/*.coffee
@@ -11,4 +62,12 @@ coffee -c -o ./Resources/android/ui/ ./Resources/android/ui/*.coffee
 coffee -c -o ./Resources/iphone/ ./Resources/iphone/*.coffee
 coffee -c -o ./Resources/iphone/ui/ ./Resources/iphone/ui/*.coffee
 
-make run
+printf "CoffeeScript compiled\n"
+printf "Running application\n"
+
+# Run Application
+if [ ! -z "${platform}" ] && [ "${platform}" == "android" ]; then
+	make deploy platform="${platform}"
+else
+	make run
+fi
