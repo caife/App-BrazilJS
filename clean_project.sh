5974e8e971770e1eb6fce922bcac51ad76f16f7a
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-07-25 21:12:31 -0300

    Create a shell script file to clean project

---
+++ clean_project.sh
@@ -0,0 +1,25 @@
+# App
+rm -Rf tiapp.xml
+
+# Android resources
+rm -Rf Resources/android/appicon.png
+rm -Rf Resources/android/default.png
+
+# iPhone resources
+rm -Rf Resources/iphone/Default.png
+rm -Rf Resources/iphone/Default@2x.png
+rm -Rf Resources/iphone/appicon.png
+rm -Rf Resources/iphone/appicon@2x.png
+
+# Data
+rm -Rf Resources/data/*
+
+# Images
+rm -Rf Resources/images/*
+
+# JavaScript files
+rm -Rf Resources/*.js
+rm -Rf Resources/lib/*.js
+rm -Rf Resources/ui/*.js
+rm -Rf Resources/android/ui/*.js
+rm -Rf Resources/iphone/ui/*.js
